36a9e42b8cbbe6ded6aba39f265f461cc71dee44
Vladimir Vivien <vladimir.vivien@gmail.com>
2017-08-07 22:42:47 -0400

    listing pods

---
+++ listpods.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	if len(pods.Items) > 0 {
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		for _, pod := range pods.Items {
+			fmt.Printf("pod %s\n", pod.GetName())
+		}
+	} else {
+		fmt.Println("No pods found!")
+	}
+}
